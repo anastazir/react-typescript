e98a2ce4177c494e3a960f84b0350e2fa86db22a
Anas Tazir <atazir2018@gmail.com>
2021-09-05 14:52:25 +0530

    clear inputs after submission

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import {ITask} from "./Interfaces"
 
 const App: FC= () => {
-  const [task, setTask] = useState<string>('initialState')
+  const [task, setTask] = useState<string>('')
   const [deadline, setDeadline] = useState<number>(0)
   const [todoList, setTodoList] = useState<ITask[]>([]) // accepts a object ITask which must be an array
 
@@ -20,14 +20,17 @@ const App: FC= () => {
     const newTask = {taskName: task, deadline: deadline}
     console.log(newTask);
     setTodoList([...todoList, newTask])
+    // empty the inputs on click
+    setTask("")
+    setDeadline(0)
   }
 
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." name="task" onChange={handleChange} />
-          <input type="number" placeholder="Deadline in days" name="deadline" onChange={handleChange} />
+          <input type="text" placeholder="Task..." name="task" onChange={handleChange} value={task} />
+          <input type="number" placeholder="Deadline in days" name="deadline" onChange={handleChange} value={deadline} />
         </div>
         <button onClick={addTask}>Add</button>
       </div>
