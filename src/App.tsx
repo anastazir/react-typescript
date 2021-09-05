a5076bd900a8016ba058b9ae8328073362e0ce3e
Anas Tazir <atazir2018@gmail.com>
2021-09-05 14:31:44 +0530

    intialize useState and handle changes for inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import {FC} from "react"
+import {ChangeEvent, FC, useState} from "react"
 import './App.css';
 
 const App: FC= () => {
+  const [task, setTask] = useState<string>('initialState')
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todo, setTodo] = useState([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void =>{
+    if (event.target.name === 'task'){
+      setTask(event.target.value)
+    }
+    else{
+      setDeadline(Number(event.target.value))
+    }
+  }
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
-          <input type="number" placeholder="Deadline in days" />
+          <input type="text" placeholder="Task..." name="task" onChange={handleChange} />
+          <input type="number" placeholder="Deadline in days" name="deadline" onChange={handleChange} />
         </div>
         <button>Add</button>
       </div>
